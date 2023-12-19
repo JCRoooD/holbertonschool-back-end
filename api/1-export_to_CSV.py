0ec79b794de10080f1cc8eae5e1b3302a03c1528
JCRoooD <juancarlos-99@live.com>
2023-12-19 10:09:38 -0400

    rename directory

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import sys
+import requests
+
+
+employee_id = sys.argv[1]
+"""Get the employee ID"""
+
+u_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+""""Get the user response"""
+
+data = u_response.json()
+"""Get the user response in json format"""
+
+employee_name = data.get('name')
+"""Get the employee name"""
+
+to_do_response = requests.get(
+    f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+"""Get the to do response"""
+
+to_do_data = to_do_response.json()
+"""Get the to do response in json format"""
+
+to_do_total = len(to_do_data)
+"""Get the total number of to do"""
+
+complete_to_do = sum([1 for task in to_do_data if task.get('completed')])
+"""Get the number of completed to do"""
+
+print(
+    f'Employee {employee_name} is done with tasks({complete_to_do}/{to_do_total}):')
+
+for task in to_do_data:
+    if task.get('completed'):
+        print('\t {}'.format(task.get('title')))
+"""Print the completed tasks"""
+
+with open('USER_ID.csv', 'w') as file:
+    for task in to_do_data:
+        file.write('"{}","{}","{}","{}"\n'.format(
+            employee_id, employee_name, task.get('completed'), task.get('title')))
+
+
+if __name__ == '__main__':
+    pass
