c05fef1d955bb58c80e35f37ef524750b4e9fc6d
JCRoooD <juancarlos-99@live.com>
2023-12-19 10:52:23 -0400

    fixed pycodestyle

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -19,7 +19,7 @@ employee_name = data.get('name')
 """Get the employee name"""
 
 to_do_response = requests.get(
-    f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
+    'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
 """Get the to do response"""
 
 to_do_data = to_do_response.json()
@@ -31,12 +31,12 @@ to_do_total = len(to_do_data)
 complete_to_do = sum([1 for task in to_do_data if task.get('completed')])
 """Get the number of completed to do"""
 
-print(
-    f'Employee {employee_name} is done with tasks({complete_to_do}/{to_do_total}):')
+print("Employee " + employee_name + " is done with tasks(" +
+      complete_to_do + "/" + to_do_total + "):")
 
 for task in to_do_data:
-    if task.get('completed'):
-        print('\t {}'.format(task.get('title')))
+    if task['completed']:
+        print('\t ' + task['title'])
 
 if __name__ == '__main__':
     pass
