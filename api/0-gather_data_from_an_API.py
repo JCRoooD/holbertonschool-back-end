2b4c456721bfe3918038d4c8a20eb5ede73b0d72
JCRoooD <juancarlos-99@live.com>
2023-12-19 10:52:41 -0400

    added comment modeul

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
-
+"""
+Python script that, using this REST API,
+for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 import sys
 import requests
 
